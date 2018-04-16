28e7005251934f8666cd763999d3770df05afc5f
YURENJIE <630573232@qq.com>
2018-04-16 09:22:36 +0800

    no message

---
+++ YRJSwiftKit/YRJSwiftKit/YRJSwiftKit/YRJKit.swift
@@ -0,0 +1,12 @@
+//
+//  YRJKit.swift
+//  YRJSwiftKit
+//
+//  Created by YURENJIE on 2018/4/12.
+//  Copyright © 2018年 yurenjie. All rights reserved.
+//
+
+import UIKit
+
+let SCREEN_WIDTH = UIScreen.main.bounds.size.width
+let SCREEN_HEIGHT = UIScreen.main.bounds.size.height
